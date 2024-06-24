387007a5b94d3ed877a8926f27091c24ad5db739
Juan carlos chocllu <j.chocllu@opti-system.com>
2024-06-24 15:33:46 -0400

    validacion de todos los campos

--- src/autenticacion/dto/create-autenticacion.dto.ts
+++ src/autenticacion/dto/create-autenticacion.dto.ts
@@ -1,55 +1,46 @@
-import {IsString, IsNotEmpty, IsOptional, IsEmail,IsEnum } from 'class-validator'
-import { Genero, Rol } from '../enums/autenticacion.enum'
+import { IsString, IsNotEmpty, IsEmail, IsEnum, IsOptional } from 'class-validator';
+import { Genero, Rol } from '../enums/autenticacion.enum';
+
 export class CreateAutenticacionDto {
   
-    @IsString()
-    @IsNotEmpty({message:'Este campo es obligatorio'})
-    cedulaIdentidad: string
+  @IsString()
+  @IsNotEmpty({ message: 'La cédula de identidad es obligatoria' })
+  cedulaIdentidad: string;
 
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  nombres: string;
 
-    @IsString()
-    @IsNotEmpty({message:'Este campo es obligatorio'})
-    nombres: string
+  @IsString()
+  @IsNotEmpty({ message: 'Los apellidos son obligatorios' })
+  apellidos: string;
 
-    
-    @IsString()
-    @IsNotEmpty({message:'Este campo es obligatorio'})
-    apellidos: string
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre de usuario es obligatorio' })
+  username: string;
 
+  @IsString()
+  @IsNotEmpty({ message: 'La contraseña es obligatoria' })
+  password: string;
 
-    @IsString()
-    @IsNotEmpty({message:'Este campo es obligatorio'})
-    username: string
+  @IsString()
+  @IsNotEmpty({ message: 'El correo electrónico es obligatorio' })
+  @IsEmail({}, { message: 'El correo electrónico debe ser válido' })
+  email: string;
 
+  @IsString()
+  @IsNotEmpty({ message: 'El número de teléfono es obligatorio' })
+  telefono: string;
 
-    @IsString()
-    @IsNotEmpty({message:'Este campo es obligatorio'})
-    password: string
+  @IsString()
+  @IsNotEmpty({ message: 'La dirección es obligatoria' })
+  direccion: string;
 
- 
-    @IsOptional()
-    @IsEmail()
-    email: string
+  @IsEnum(Genero)
+  @IsNotEmpty({ message: 'El género es obligatorio' })
+  genero: Genero;
 
-    @IsString()
-    @IsOptional()
-    telefono: string
-
-
-
-    @IsString()
-    @IsOptional()
-    direccion: string
-
-
-    @IsEnum(Genero)
-    @IsString()
-    @IsNotEmpty({message:'Este campo es obligatorio'})
-    genero: string
-
-   
-    @IsString()
-    @IsEnum(Rol)
-    @IsOptional()
-    rol: string
+  @IsEnum(Rol)
+  @IsOptional()
+  rol: Rol;
 }
